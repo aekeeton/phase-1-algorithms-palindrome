ef4015cbd60b1aa04f47d79d8e76ffc5212c2a87
aekeeton <ashley.keeton@outlook.com>
2022-07-02 14:31:25 -0400

    commit tree 6f597bfad6cc1546c72235318738f50a344cbca8

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.split('').reverse().join('') === word) {
+    return true 
+  }
+  else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
 */
+
+// The function accepts one argument that is a string.
+// It checks if it's a palindrome and will return true if it is, and false if it's not.
+// The string has to be reversed. 
+  // To do that, we need to use split() to return a new array.
+  // with the new array, we can use the reverse() method.
+  // then we'll need to use join() to put the reversed array back into a string.
+  
+  // If the reversed string deeply equals the argument, it should return true.
+  // If the reversed string does not deeoly equal the argument, it should return false.
 
 /*
   Add written explanation of your solution here
